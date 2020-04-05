c4849c458b85608128b2f00758e23842dadf0cfe
lvachon <lvachon@gmail.com>
2020-04-05 13:04:55 -0400

    Vid 2 strip ppm

---
+++ vid2strip.php
@@ -0,0 +1,19 @@
+<?php
+$fname = $argv[1];
+$COLS = 31;
+$ROWS = 15;
+//convert to frames
+exec("ffmpeg -i {$fname} -vf scale={$COLS}:{$ROWS} -q:v 1 temp%5d.png");
+//Assemble frames
+$a = glob("temp*.png");
+$frames = count($a);
+
+$im = imagecreatetruecolor($COLS,$ROWS*$frames);
+for($frame=0;$frame<$frames;$frame++){
+	$in = imagecreatefrompng($a[$frame]);
+	imagecopy($im,$in,0,$frame*$ROWS,0,0,$COLS,$ROWS);
+}
+$outname = explode(".",basename($fname))[0];
+imagepng($im,"{$outname}_strip.png");
+exec("magick convert {$outname}_strip.png -compress none {$outname}_strip.ppm");
+exec("del temp*.png");
