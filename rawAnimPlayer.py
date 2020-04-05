c7c6061313fb3fe0005dd86594203e712b84d728
Luc Vachon <lvachon@gmail.com>
2020-04-05 12:40:10 -0400

    bugfix and raw testfile

--- rawAnimPlayer.py
+++ rawAnimPlayer.py
@@ -18,7 +18,7 @@ with open(sys.argv[1],"rb") as file:
 	animation = bytearray(file.read())
 
 frame=0;
-frames = len(animation)/(3*num_pixels)
+frames = (int)(len(animation)/(3*num_pixels))
 while(1):
 	neopixel_write.neopixel_write(pin,animation[3*num_pixels*frame:3*num_pixels*(frame+1)])
 	frame=(frame+1)%frames
