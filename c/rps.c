c9fdaafd775afb5546bc2279130484ce5051ddaa
Luc Vachon <lvachon@gmail.com>
2020-04-09 19:08:37 -0400

    better...

--- c/rps.c
+++ c/rps.c
@@ -141,26 +141,28 @@ ws2811_led_t ROCK = 0x00FF0000;
 ws2811_led_t PAPER = 0x0000FF00;
 ws2811_led_t SCISSORS = 0x000000FF;
 
-void setLED(i,c){
+void setLED(int i, ws2811_led_t c){
     ledstring.channel[0].leds[i]=c;
 }
 
 void compCell(int i){
     ws2811_led_t myType = grid[i];
-    int r,p,s = 0;
+    int r=0;
+    int p=0;
+    int s=0;
     for(int j=0;j<9;j++){
         ws2811_led_t n = getNeighbor(i,j);
         if(n==ROCK){r++;continue;}
         if(n==PAPER){p++;continue;}
         if(n==SCISSORS){s++;continue;}
     }
-    if(myType==ROCK && p>r && p>s){
+    if(myType==ROCK && p>s){
         setLED(i,PAPER);
     }
-    if(myType==PAPER && s>r && s>p){
+    if(myType==PAPER && s>r){
         setLED(i,SCISSORS);
     }
-    if(myType==SCISSORS && r>p && r>s){
+    if(myType==SCISSORS && r>p){
         setLED(i,ROCK);
     }
 }
@@ -183,20 +185,30 @@ int main(){
         if(rnd==0){ledstring.channel[0].leds[i]=ROCK;continue;}
         if(rnd==1){ledstring.channel[0].leds[i]=PAPER;continue;}
         if(rnd==2){ledstring.channel[0].leds[i]=SCISSORS;continue;}
+	i--;
     }
 
     while(running){
-        for(int i=0;i<LED_COUNT;i++){//Copy framebuffer back to memory for next step
-            grid[i]=ledstring.channel[0].leds[i];
+        for(int frame=0;frame<1800 && running;frame++){
+	    for(int i=0;i<LED_COUNT;i++){//Init LEDS to a random state
+	        unsigned int rnd = rand()&3;
+	        if(rnd==0){ledstring.channel[0].leds[i]=ROCK;continue;}
+	        if(rnd==1){ledstring.channel[0].leds[i]=PAPER;continue;}
+	        if(rnd==2){ledstring.channel[0].leds[i]=SCISSORS;continue;}
+	        i--;
+	    }
+	    for(int i=0;i<LED_COUNT;i++){//Copy framebuffer back to grid for next step
+                grid[i]=ledstring.channel[0].leds[i];
+            }
+            for(int i=0;i<LED_COUNT;i++){//Compute next step, saving it into framebuffer
+                compCell(i);
+            }
+            if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS){//Render that framebuffer
+                fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
+                break;
+            }
+            usleep(1000000 / 30);
         }
-        for(int i=0;i<LED_COUNT;i++){//Compute next step, saving it into framebugge
-            compCell(i);
-        }
-        if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS){//Render that framebuffer
-            fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
-            break;
-        }
-        usleep(1000000 / 30);
     }
     ws2811_fini(&ledstring);
 
