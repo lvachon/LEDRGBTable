1fc466fa2e7fa3889a335ca4db8dfa68be802d9e
lvachon <lvachon@gmail.com>
2020-05-23 18:08:47 -0400

    raw 2 ascii for imagemagick

--- web/img.php
+++ web/img.php
@@ -2,8 +2,8 @@
 $url = $argv[1];
 $brightness = $argv[2];
 exec("../c/rawPlayer loading.raw 1 {$brightness}");
-exec("rm -f img.ppm; rf -f img.raw");
-exec("convert \"{$url}\" -thumbnail 31x16^ -gravity center -extent 31x16  img.ppm");
+exec("rm -f img.ppm; rm -f img.raw");
+exec("convert \"{$url}\" -thumbnail 31x16^ -gravity center -extent 31x16  -compress none img.ppm");
 exec("../c/rawPlayer processing.raw 1 {$brightness}");
 exec("python3 ../vid_conv/ppm2bytes.py img.ppm img.raw");
 exec("../c/rawPlayer img.raw 1 {$brightness}");
