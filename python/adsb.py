880f70455c7cd70305d366466e8a4069a7ff431c
lvachon <lvachon@gmail.com>
2020-07-12 01:43:44 -0400

    adsb map

---
+++ python/adsb.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import board
+import neopixel_write
+import digitalio
+import time
+import math
+import sys
+import urllib.request
+import json
+
+pin = digitalio.DigitalInOut(board.D18)
+pin.direction = digitalio.Direction.OUTPUT
+
+
+COLS = 31
+ROWS = 16
+NORTH = 43.4693
+SOUTH = 40.4541
+EAST = -68.2321
+WEST = -74.0321
+num_pixels = COLS*ROWS
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1.0, auto_write=False, pixel_order=ORDER)
+def hue2rgb(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos*3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos*3)
+        g = 0
+        b = int(pos*3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos*3)
+        b = int(255 - pos*3)
+    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
+
+with open("neadsb.raw","rb") as file:
+	animation = bytearray(file.read())
+cleananim[:] = animation
+for frame in range(0:30):
+	animation[:]=cleananim
+	with urllib.request.urlopen('http://192.168.1.21:8080/data/aircraft.json') as response:
+		res = response.read()
+		print(res)
+		planes = json.loads(res)["aircraft"]
+		for plane in aircraft:
+			x = int(COLS*(float(aircraft["lon"])-WEST)/(EAST-WEST))
+			y = int(ROWS-ROWS*(float(aircraft["lat"])-SOUTH)/(NORTH-SOUTH))
+			c = int(255*float(aircraft["alt_baro"])/45000)
+			h = hue2rgb(c)
+			animation[x+y*COLS]=h[0]
+			animation[x+y*COLS+1]=h[1]
+			animation[x+y*COLS+2]=h[2]
+		neopixel_write.neopixel_write(pin,animation)
+		time.sleep(5)
