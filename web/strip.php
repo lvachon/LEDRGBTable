ee8a6a257774183abea5a912923afad0ad6d1a3f
lvachon <lvachon@gmail.com>
2020-07-14 19:30:04 -0400

    move the conversion work over to the server

--- web/strip.php
+++ web/strip.php
@@ -2,7 +2,10 @@
 $url = $argv[1];
 $brightness = $argv[2];
 
-exec("convert \"{$url}\" -compress none -set comment 'strip.php4LEDRGBTable' strip.ppm");
-exec("python3 ../vid_conv/ppm2bytes.py strip.ppm strip.raw");
-exec("../c/rawPlayer strip.raw 1 {$brightness}");
-file_put_contents("webcmd","../c/rawPlayer strip.raw 1 {$brightness}");
+//exec("convert \"{$url}\" -compress none -set comment 'strip.php4LEDRGBTable' strip.ppm");
+//exec("python3 ../vid_conv/ppm2bytes.py strip.ppm strip.raw");
+file_put_contents("strip.raw",file_get_contents($url));
+$frames = filesize("strip.raw")/(3*31*16);
+$loops = floor(900/$frames);
+exec("../c/rawPlayer strip.raw {$loops} {$brightness}");
+//file_put_contents("webcmd","../c/rawPlayer strip.raw 1 {$brightness}");
